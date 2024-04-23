1f0691574616820378524f1666c2fd70bf13caf8
Bartu Özcan <bartu.ozcan2004@gmail.com>
2024-04-23 18:17:38 +0300

    updated rf_main

--- src/RF/rf_main.cpp
+++ src/RF/rf_main.cpp
@@ -1,21 +1,24 @@
 #include <Arduino.h>
-#include <EByte_LoRa_E32_library.h>
+
+#define E32_TTL_1W
+
+#include <LoRa_E32.h>
 
 void setsMainOpt();
 void rfInit();
 
-LoRa_E32 e32ttl100(&Serial2);
+LoRa_E32 e32ttl1w(&Serial2);
 
 void rfInit(){
     
-    e32ttl100.begin();
+    e32ttl1w.begin();
 }
 
 void setsMainOpt(){
     // Bu Function RF'in başlangıçtaki temel ayarlarını yapmaktadır.
 
     ResponseStructContainer ayar;
-    ayar = e32ttl100.getConfiguration();
+    ayar = e32ttl1w.getConfiguration();
 
     Configuration conf = *(Configuration*) ayar.data;
 
@@ -26,11 +29,15 @@ void setsMainOpt(){
     conf.OPTION.fec = FEC_1_ON;
     conf.OPTION.fixedTransmission = FT_TRANSPARENT_TRANSMISSION;
     conf.OPTION.ioDriveMode = IO_D_MODE_PUSH_PULLS_PULL_UPS;
-
+    conf.OPTION.wirelessWakeupTime = WAKE_UP_250;
+    conf.OPTION.transmissionPower = POWER_30;
 
     conf.SPED.airDataRate = AIR_DATA_RATE_011_48;
     conf.SPED.uartBaudRate = UART_BPS_9600;
     conf.SPED.uartParity = MODE_00_8N1;
+
+    
+
 
 
 
