94f730a3359299acf7a3e8fa360a24e4923dd9bf
Bartu Özcan <bartu.ozcan2004@gmail.com>
2024-04-23 23:12:30 +0300

    main is modified

--- src/main.cpp
+++ src/main.cpp
@@ -1,14 +1,10 @@
 #include <Arduino.h>
 #include "rf_main.h"
 
-// put function declarations here:
-int myFunction(int, int);
-
 void setup() {
-  // put your setup code here, to run once:
   rfInit();
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+
 }
